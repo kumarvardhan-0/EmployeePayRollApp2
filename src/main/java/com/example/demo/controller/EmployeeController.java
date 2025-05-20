ebf5269b3e1dabc32f5d6c006f0ad12e989bdd39
Kumarvardhan <gajulakumarvardhan230@gmail.com>
2025-05-20 14:22:38 +0530

    UC1-Set up Done

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Employee Payroll App";
+    }
+}
